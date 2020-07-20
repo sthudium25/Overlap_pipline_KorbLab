1e3cdc8ec40d6eac21f9c315734459aaaa0c02c5
sthudium25 <sam.thudium1@gmail.com>
2020-07-20 15:21:43 -0400

    This is a basic pipeline for overlapping an arbitrary number of lists.  It will produce an overlap output for all possible pairs of lists (text files) in a provided folder.

---
+++ Overlap_pipeline.R
@@ -0,0 +1,21 @@
+## Overlap analaysis pipeline for a folder containing the lists of genes to be overlapped.
+
+## set working directory as the folder that contain the genelist files 
+setwd("/Users/Sammy/Desktop/UPenn/Projects/Overlap_pipline/test_genelists")
+files <- dir()
+
+for( i in files ){
+  D <- data.frame(matrix(nrow=0,ncol=1))
+  d <- read.table(i,header=F,stringsAsFactors=F)
+  for( j in files) {
+    if (j > i) {
+      d2 <- read.table(j, header = F, stringsAsFactors = F)
+      overlap <- d %>% intersect(d2)
+      OL_df <- paste("D", i, j, sep = "_")
+      OL_df<- assign(OL_df, rbind(D,overlap))
+      myfile <- file.path("/Users/Sammy/Desktop/UPenn/Projects/Overlap_pipline/OL_outputs", 
+                          paste0("OL", "_", i, "_", j, ".txt"))
+      write_tsv(OL_df, path = myfile, col_names = F)
+    }
+  }
+}
