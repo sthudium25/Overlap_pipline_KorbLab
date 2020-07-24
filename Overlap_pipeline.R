7158b29f9e5bd39e41efec07a4b295701520ce14
sthudium25 <sam.thudium1@gmail.com>
2020-07-24 11:12:17 -0400

    Changing the output file creation method. Beginning an Rmd as well.

--- Overlap_pipeline.R
+++ Overlap_pipeline.R
@@ -1,9 +1,10 @@
 ## Overlap analaysis pipeline for a folder containing the lists of genes to be overlapped.
-
+library(tidyverse)
 ## set working directory as the folder that contain the genelist files 
 setwd("/Users/Sammy/Desktop/UPenn/Projects/Overlap_pipline/test_genelists")
 files <- dir()
 
+dir.create(path = "../output")
 for( i in files ){
   D <- data.frame(matrix(nrow=0,ncol=1))
   d <- read.table(i,header=F,stringsAsFactors=F)
@@ -13,7 +14,7 @@ for( i in files ){
       overlap <- d %>% intersect(d2)
       OL_df <- paste("D", i, j, sep = "_")
       OL_df<- assign(OL_df, rbind(D,overlap))
-      myfile <- file.path("/Users/Sammy/Desktop/UPenn/Projects/Overlap_pipline/OL_outputs", 
+      myfile <- file.path("../output", 
                           paste0("OL", "_", i, "_", j, ".txt"))
       write_tsv(OL_df, path = myfile, col_names = F)
     }
