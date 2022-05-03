51a07ef959409b16214d5fd1e48dce2f4772539e
sthudium25 <sam.thudium1@gmail.com>
2022-05-03 17:35:54 -0400

    Reworked the overlap method completely:  - Removed unnecessary reading-in of input files  - Vectorized the pairwise intersection of lists  - Output file names are still awkward, but better than in previous versions.

--- Overlap_pipeline_git_master.R
+++ Overlap_pipeline_git_master.R
@@ -3,60 +3,65 @@
 ## in each of the original lists and overlap lists, as well as statistics, and 
 ## a folder containing pairwise venn diagrams (pdfs)
 library(tidyverse)
+library(glue)
 
+## RUN THESE FUNCTIONS BUT DON'T CHANGE
+hyperGeomTest <- function(overlap, sample1, sample2, background) {
+  sum(dhyper(overlap:sample1, sample2, background - sample2, sample1))
+}
+
+calculateOverlaps <- function(gene.lists, backgroundSize = 18000) {
+  nms <- combn( names(gene.lists) , 2 , FUN = paste0 , collapse = "_" , simplify = FALSE )
+  ll <- combn( gene.lists , 2 , simplify = FALSE )
+  out.OLnum <- lapply( ll , function(x) length( intersect( x[[1]] , x[[2]] ) ) )
+  out.OLgenes <- lapply( ll , function(x) intersect( x[[1]] , x[[2]] ) ) 
+  out.OLgenes = setNames( out.OLgenes , nms )
+  
+  for (i in 1:length(out.OLgenes)) {
+    filename = names(out.OLgenes)[i]
+    write_tsv(as_tibble(out.OLgenes[[i]]), file = glue('../geneOverlapLists/{filename}.txt'), col_names = F)
+  }
+  
+  List1 = unlist(lapply(ll, function(x) names(x)[1]))
+  List2 = unlist(lapply(ll, function(x) names(x)[2]))
+  ith = unlist(lapply(ll, function(x) length(x[[1]])))
+  jth = unlist(lapply(ll, function(x) length(x[[2]])))
+  overlap = unlist(lapply(out.OLnum, '[', 1))
+  
+  tmp = cbind(List1, List2, ith, jth, overlap) %>% as.data.frame()
+  
+  out <- tmp2 %>% 
+    mutate(across(ith:overlap, .fns = as.numeric),
+           hyper_pval = mapply(hyperGeomTest, overlap, ith, jth, backgroundSize))
+  out
+}
 ## set working directory as the folder that contain the genelist files 
-setwd("/Users/Sammy/Desktop/UPenn/Projects/DOT1L/Dot1L_H3.3ChIP_analysis/genelists")
+setwd("/Users/Sammy/Desktop/UPenn/Projects/ASD_genes/Experiments/68.ReviewerResponseExperiment/recount3_forMouseStudies/OverlapAnalysis_wSignatureLists/genelists")
 
 files <- dir()
+
+gene.lists <- list()
+sample.names <- c()
+
+for (i in files) {
+  sample = str_remove(i, '\\..*')
+  tmp = read_tsv(i, col_names = F) %>% pull(X1)
+  sample.names = c(sample.names, sample)
+  gene.lists = c(gene.lists, list(tmp))
+}
+
+names(gene.lists) = sample.names
 
 ## The empty dataframe created below has five columns: The first two name the lists that went into the 
 ## overlap, and columns 3-5 contain the number of genes in the ith list, jth list, and the number of genes
 ## shared by the ith and jth lists, respectively.
 
-## The rows of the dataframe correspond to the number of overlaps performed, which can be calculated
-## for a given number of lists, n: (n * (n-1)) / 2
-n_files <- length(files)
-n_minus_1 <- n_files - 1
-listLength <- (n_files * n_minus_1) / 2
+dir.create(path = "../geneOverlapLists")
 
-OLValuesDf <- data.frame(matrix(nrow = listLength, ncol = 5))
-rowIdx <- 1
-names(OLValuesDf) <- c("List1", "List2", "ith", "jth", "overlap")
-dir.create(path = "../output")
-for( i in files ){
-  D <- data.frame(matrix(nrow=0,ncol=1))
-  d <- read.table(i,header=F,stringsAsFactors=F)
-  for( j in files) {
-    if (j > i) {
-      d2 <- read.table(j, header = F, stringsAsFactors = F)
-      overlap <- d %>% intersect(d2)
-      OL_df <- paste("D", i, j, sep = "_")
-      OL_df<- assign(OL_df, rbind(D,overlap))
-      myfile <- file.path("../output", 
-                          paste0("OL", "_", i, "_", j, ".txt"))
-      write_tsv(OL_df, path = myfile, col_names = F)
-      OLValuesDf[rowIdx, ] <- c(i, j, count(d), count(d2), count(overlap))
-      rowIdx <-  rowIdx + 1
-    }
-  }
-}
+OLValuesDf <- calculateOverlaps(gene.lists, 18000)
 
-## Change the totalPop to match the background expressed genelist for your experiment
-totalPop <- 18000
 
-hyperGeomTest <- function(overlap, sample1, sample2, background) {
-  sum(dhyper(overlap:sample1, sample2, background - sample2, sample1))
-}
-
-OLValuesDf <- OLValuesDf %>% mutate(hyper_pval = mapply(hyperGeomTest, 
-                                                        OLValuesDf$overlap, 
-                                                        OLValuesDf$ith, 
-                                                        OLValuesDf$jth, 
-                                                        totalPop))
-OLValuesDf$List1 <- gsub("\\..*", "", OLValuesDf$List1)
-OLValuesDf$List2 <- gsub("\\..*", "", OLValuesDf$List2)
-
-write_csv(OLValuesDf, path = "../OverlapsTable_HypergeometricTestPvals.csv")
+write_csv(OLValuesDf, path = "../OverlapsResultsTable.csv")
 
 library(VennDiagram)
 list1 <- split(OLValuesDf, rownames(OLValuesDf))
